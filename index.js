16281dd366d3ad5b2daeb3c035313928c0e33c74
erobbi <elirobbins0@gmail.com>
2021-06-19 16:09:11 -0400

    completed assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+const string = "Hello!";
+
+function shout(string) {
+    return 'hello'.toUpperCase();
+}
+
+function whisper(string) {
+    return 'hello'.toLowerCase();
+}
+
+function logShout(string) {
+    console.log('hello'.toUpperCase());
+    return 
+}
+
+function logWhisper(string) {
+    console.log('hello'.toLowerCase());
+    return 
+}
+
+function sayHiToGrandma(string) {
+    if (string == string.toLowerCase())
+        return console.log("I can\'t hear you!");
+    if (string == 'HELLO')
+        return console.log("YES INDEED!")
+    if (string == 'I love you, Grandma.')
+        return console.log("I Love you, too.")
+
+}
